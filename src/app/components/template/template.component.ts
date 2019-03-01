6ddf920f500d3328699fa96c383f948442fbb299
Marco Aneloa <marco.aneloa@msp.gob.ec>
2019-03-01 10:13:32 -0500

    Formularios en Angular

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -5,19 +5,33 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: [`
-  .ng-invalid.ng-touched:not(form) {
-    border:1px solid red;
-  }
-  `]
+  styles: [//`
+  //.ng-invalid.ng-touched:not(form) {
+  //  border:1px solid red;
+  //}
+  //`
+]
 })
 export class TemplateComponent implements OnInit {
 
   usuario:Object={
     nombre:null,
     apellido:null,
-    email:null
+    email:null,
+    pais:"124",
+    sexo:"Hombre",
+    acepta:false
   }
+
+  paises=[{
+    codigo:"123",
+    nombre:"Ecuador"
+  },{
+    codigo:"124",
+    nombre:"Perú"
+  }];
+
+  sexos:string[] = ["Hombre","Mujer"];
 
   constructor() { }
 
